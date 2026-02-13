07a78b3f115146c5fcf87fc1784a921eb30dc103
rasmus <ramus.puromaki@hotmail.fi>
2026-02-14 00:37:22 +0200

    Push all the scalar and other plugins

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcb0f62b0da36b5385eaa04a8d8ba5c3ea6942e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10104294ad8a8d4b66274ad8bd3705f7fcd3d77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
